9b5df037fa79e088dea31b96f8132faca6cbf732
daiki0904 <daiki.yamane@di-v.co.jp>
2021-05-20 16:44:23 +0900

    バリデーション&エラーメッセージ

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :validatable
+  
+  with_options presence: true do
+    validates :nickname
+    validates :encrypted_password, format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/}
+    with_options format:{with: /\A[ぁ-んァ-ヶ一-龥々ー]/} do
+      validates :last_name
+      validates :family_name
+    end
+    with_options format:{with: /\A[ァ-ヶー－]+\z/} do
+      validates :kana_last_name
+      validates :kana_family_name
+    end
+    validates :birth_day
+  end
 end
