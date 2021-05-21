9646f257525776abc354fab516d3c56b34a56bb2
daiki0904 <daiki.yamane@di-v.co.jp>
2021-05-21 17:01:51 +0900

    モデルの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,10 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+  
+  def item_params
+    params.require(:item).permit(:image,:item_name,:information,:category_id,:status_id,:shipping_fee_id,:prefecture_id,:schedule_id,:price).merge(user_id: current_user.id)
+  end
 end
