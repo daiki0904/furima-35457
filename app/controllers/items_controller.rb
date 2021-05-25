57553ed9492bb0155d99dd3235947425ec82469d
daiki0904 <daiki.yamane@di-v.co.jp>
2021-05-25 14:32:10 +0900

    商品一覧機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   def index
-    # @items = Item.all
+    @items = Item.all.order(created_at: :desc)
   end
 
   def new
@@ -20,7 +20,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image, :item_name, :information, :category_id, :status_id, :shipping_fee_id, :prefecture_id,
-                                 :schedule_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :item_name, :information,:category_id, :status_id, :shipping_fee_id, :prefecture_id, :schedule_id, :price).merge(user_id: current_user.id)
   end
 end
