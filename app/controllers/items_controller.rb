d792e1f6bf08cbd2b411b70eebddc2ae82d5592a
daiki0904 <daiki.yamane@di-v.co.jp>
2021-05-25 16:26:44 +0900

    商品詳細表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,10 +20,10 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
-  
+
   private
 
   def item_params
-    params.require(:item).permit(:image, :item_name, :information,:category_id, :status_id, :shipping_fee_id, :prefecture_id, :schedule_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :item_name, :information, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :schedule_id, :price).merge(user_id: current_user.id)
   end
 end
