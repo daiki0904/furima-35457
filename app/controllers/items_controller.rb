3504d7d4203ee47d1e0d6c6ba5e12764e3af8e49
daiki0904 <daiki.yamane@di-v.co.jp>
2021-05-27 13:42:52 +0900

    削除機能　再修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, :except [:indec, :show]
   before_action :item_find, only: [:show, :edit, :update, :destroy]
   before_action :user_check, only: [:edit, :update, :destroy]
   def index
