557b3d969915a8349b6fa17d461c4776d9184c0f
daiki0904 <daiki.yamane@di-v.co.jp>
2021-05-31 11:31:39 +0900

    クレカ決済実装（環境変数設定前）

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,8 +8,9 @@ class OrdersController < ApplicationController
   def create
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      pay_item
       @order_address.save
-      redirect_to root_path
+      return redirect_to root_path
     else
       render :index
     end
@@ -18,10 +19,19 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building, :phone).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building, :phone).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
   end
 
   def item_find
     @item = Item.includes(:order).find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_243ec528f62b15597d19cda5" 
+    Payjp::Charge.create(
+      amount: @item.price,  
+      card: order_params[:token],  
+      currency: 'jpy'             
+    )
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building, :phone, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building, :phone, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postal_code, format:{with:/\A\d{3}[-]\d{4}\z/}
@@ -9,6 +9,7 @@ class OrderAddress
     validates :phone
     validates :user_id
     validates :item_id
+    validates :token
   end
   validates :prefecture_id, numericality: { other_than: 1 }
 
