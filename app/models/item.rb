7b9dab6fc6789bb673908f7f892dabbf3a93a717
daiki0904 <daiki.yamane@di-v.co.jp>
2021-06-07 11:08:48 +0900

    モデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one :order
+  has_many :item_tag_relations
+  has_many :tags, through: :item_tag_relations
   has_many_attached :images
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
@@ -8,19 +10,4 @@ class Item < ApplicationRecord
   belongs_to :schedule
   belongs_to :shipping_fee
   belongs_to :status
-
-  with_options presence: true do
-    validates :images
-    validates :item_name
-    validates :information
-    validates :price, format: { with: /\A[0-9]+\z/ },
-                      numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-  end
-  with_options numericality: { other_than: 1 } do
-    validates :category_id
-    validates :status_id
-    validates :shipping_fee_id
-    validates :prefecture_id
-    validates :schedule_id
-  end
 end
---
+++ app/models/items_tag.rb
@@ -0,0 +1,29 @@
+class ItemsTag
+
+  include ActiveModel::Model
+  attr_accessor :item_name, :information, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :schedule_id, :price, :user_id, :name, :image
+
+  with_options presence: true do
+    validates :item_name
+    validates :information
+    validates :price, format: { with: /\A[0-9]+\z/ },
+                      numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+    validates :name
+    validates :user_id
+    validates :image
+  end
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :schedule_id
+  end
+
+  def save
+    item = Item.create( item_name: item_name, information: information, category_id: category_id, status_id: status_id, shipping_fee_id: shipping_fee_id, prefecture_id: prefecture_id, schedule_id: schedule_id, price: price, user_id: user_id, image: image)
+    tag = Tag.create(name: name)
+
+    ItemTagRelation.create(item_id: item.id, tag_id: tag.id)
+  end
+end
